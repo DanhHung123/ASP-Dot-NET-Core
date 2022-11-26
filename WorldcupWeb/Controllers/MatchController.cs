f00b8bcaa8cbbcc9ddef62f1ded3afe48cbab4dc
DanhHung123 <hungnguyena123ye@gmail.com>
2022-11-26 10:58:36 +0700

    Add project files.

---
+++ WorldcupWeb/Controllers/MatchController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorldcupWeb.Controllers
+{
+    public class MatchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Match(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+        }
+    }
+}
---
+++ WorldcupWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WorldcupWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
