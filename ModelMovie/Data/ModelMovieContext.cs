717a62cec6118fd5812058424a8cf87a80c3506e
DanhHung123 <hungnguyena123ye@gmail.com>
2022-11-27 17:15:29 +0700

    add movies web

---
+++ ModelMovie/Data/ModelMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace ModelMovie.Data
+{
+    public class ModelMovieContext : DbContext
+    {
+        public ModelMovieContext (DbContextOptions<ModelMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ ModelMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ModelMovie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
